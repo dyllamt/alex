9a41add8a60c6da6a299525cdc7870d9621ab7fc
Maxwell Dylla <maxwelldylla2020@u.northwestern.edu>
2019-01-31 14:37:11 -0800

    added workspace support for aflow
    - implemented the AFLOWFrame class

--- dataspace/workspaces/materials_api.py
+++ dataspace/workspaces/materials_api.py
@@ -1,6 +1,7 @@
 from dataspace.base import Workspace
 
 from matminer.data_retrieval.retrieve_MP import MPDataRetrieval
+from matminer.data_retrieval.retrieve_AFLOW import AFLOWDataRetrieval
 
 '''
 this module implements workspaces that handle structured data in materials
@@ -33,3 +34,31 @@ class MPFrame(Workspace, MPDataRetrieval):
 
         self.memory = self.get_dataframe(criteria, properties,
                                          index_mpid=index_mpid, **kwargs)
+
+
+class AFLOWFrame(Workspace, AFLOWDataRetrieval):
+    '''
+    wrapper for making AFLOWDataRetrieval a Workspace
+    '''
+
+    def __init__(self):
+        Workspace.__init__(self)
+        AFLOWDataRetrieval.__init__(self)
+
+    def to_storage(self):
+        '''
+        transfer data from memory to storage
+        '''
+
+        raise NotImplementedError("to_storage() is not defined!")
+
+    def from_storage(self, criteria, properties, files=[],
+                     request_size=100000, request_limit=0, index_auid=False):
+        '''
+        transfer data from storage to memory. see the matminer MPDataRetrieval
+        class for optional arguments to pass to the get_dataframe method
+        '''
+
+        self.memory = self.get_dataframe(
+            criteria, properties, files=files, request_size=request_size,
+            request_limit=request_limit, index_auid=index_auid)
