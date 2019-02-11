63ae10e6952e8c4d76b4b43db168b8e1f179256b
Maxwell Dylla <maxwelldylla2020@u.northwestern.edu>
2019-02-11 15:42:57 -0800

    added default mongodb directory

--- dataspace/workspaces/materials_api.py
+++ dataspace/workspaces/materials_api.py
@@ -1,64 +1,41 @@
 from dataspace.base import Workspace
 
-from matminer.data_retrieval.retrieve_MP import MPDataRetrieval
-from matminer.data_retrieval.retrieve_AFLOW import AFLOWDataRetrieval
+from matminer.data_retrieval.retrieve_base import BaseDataRetrieval
 
-'''
-this module implements workspaces that handle structured data in materials
-database that is serviced by an api. the workspaces are essentially wrappers
-around childeren of the matminer BaseDataRetrieval class
-'''
+"""Implements workspaces that handle structured data in materials databases
+that are serviced by APIs. The workspaces are essentially wrappers around
+children of the matminer BaseDataRetrieval class.
+"""
 
 
-class MPFrame(Workspace, MPDataRetrieval):
-    '''
-    wrapper for making MPDataRerieval a Workspace
-    '''
+class APIFrame(Workspace):
+    """Wrapes BaseDataRetrieval sub-classes from matminer.
+    """
+    def __init__(self, RetrievalSubClass, **kwargs):
+        """Initializes an istance from a BaseDataRetrieval subclass.
 
-    def __init__(self, api_key=None):
-        Workspace.__init__(self)
-        MPDataRetrieval.__init__(self, api_key=api_key)
-
-    def to_storage(self):
-        '''
-        transfer data from memory to storage
-        '''
-
-        raise NotImplementedError("to_storage() is not defined!")
-
-    def from_storage(self, criteria, properties, index_mpid=False, **kwargs):
-        '''
-        transfer data from storage to memory. see the matminer MPDataRetrieval
-        class for optional arguments to pass to the get_dataframe method
-        '''
-
-        self.memory = self.get_dataframe(criteria, properties,
-                                         index_mpid=index_mpid, **kwargs)
-
-
-class AFLOWFrame(Workspace, AFLOWDataRetrieval):
-    '''
-    wrapper for making AFLOWDataRetrieval a Workspace
-    '''
-
-    def __init__(self):
-        Workspace.__init__(self)
-        AFLOWDataRetrieval.__init__(self)
+        Args:
+            RetrievalSubClass: (BaseDataRetrieval) A subclass which defines a
+                get_dataframe method for retrieving data from an API.
+            kwargs: Optional keyword arguments used to construct an instance of
+                the RetrievalSubClass class.
+        """
+        if not issubclass(RetrievalSubClass, BaseDataRetrieval):
+            raise Exception(
+                'The retriever must be an a subclass of BaseDataRetrieval!')
+        else:
+            Workspace.__init__(self)
+            self.connection = RetrievalSubClass(**kwargs)
 
     def to_storage(self):
-        '''
-        transfer data from memory to storage
-        '''
-
+        """Transfers data from memory to storage.
+        """
         raise NotImplementedError("to_storage() is not defined!")
 
-    def from_storage(self, criteria, properties, files=[],
-                     request_size=100000, request_limit=0, index_auid=False):
-        '''
-        transfer data from storage to memory. see the matminer MPDataRetrieval
-        class for optional arguments to pass to the get_dataframe method
-        '''
+    def from_storage(self, **kwargs):
+        """Collects data from storage into memory.
 
-        self.memory = self.get_dataframe(
-            criteria, properties, files=files, request_size=request_size,
-            request_limit=request_limit, index_auid=index_auid)
+        Args:
+            kwargs: Keyword arguments passed to the get_dataframe method.
+        """
+        self.memory = self.connection.get_dataframe(**kwargs)
