918783be3bc0de55c615f3062149047ace0bec62
Maxwell Dylla <maxwelldylla2020@u.northwestern.edu>
2019-05-15 13:46:38 -0500

    rework connections using remote-friendly arguments
    updated the remote_connection decorator
    updated the arguments and attributes of MongoFrame

--- dataspace/workspaces/remote_db.py
+++ dataspace/workspaces/remote_db.py
@@ -12,9 +12,8 @@ databases. currently, mongodb is supported through the pymongo interface
 
 def remote_connection(func):
     '''
-    make a remote connection and enable access to storage around an operation.
-    errors encountered while the connection is active are raised after
-    terminating the connection.
+    make a remote connection to storage around an operation. errors encountered
+    while the connection is active are raised after terminating the connection.
 
     Args:
         func (function) a function that requires access to a remote mongodb
@@ -23,8 +22,10 @@ def remote_connection(func):
     def wrapper(self, *args, **kwargs):
 
         # set-up a database connection
-        self.connection = MongoClient()[self.database][self.collection]
-        print('connected to {}.{}'.format(self.database, self.collection))
+        self.connection = MongoClient(
+            self.host, self.port, authSource=self.authSource,
+            username=self.username, password=self.password)
+        print('connected to {} @ {}'.format(self.database, self.host))
 
         # execute operation
         error = None
@@ -33,7 +34,8 @@ def remote_connection(func):
         except Exception as e:  # put off raising errors till later
             error = e
 
-        # break connection and terminate mongod process
+        # close the database connection
+        self.connection.close()
         self.connection = None
         print('disconnected \n')
 
@@ -54,24 +56,38 @@ class MongoFrame(Workspace):
     must add the index as a column if you want to store that information!
 
     Attributes:
-        collection (str) name of a pymongo collection
-        database (str) name of a pymongo database
-        path (str) path to a mongodb directory
-        connection (Collection|None) statefull connection to storage
+        host (str) hostname or IP address or Unix domain socket path
+        port (int) port number on which to connect
+        database (str) storage database
+        collection (str) storage collection
+        authSource (str) database to authenticate against
+        username (str|None) username to authenticate with
+        password (str|None) password to authenticate with
+        connection (MongoClient|None) statefull connection to storage
         memory (DataFrame|None) pandas dataframe for temporary storage
     '''
 
-    def __init__(self, collection, database, path='/data/db'):
+    def __init__(self, host, port, database, collection, authSource=None,
+                 username=None, password=None):
         '''
         Args:
-            path (str) path to a mongodb directory
-            database (str) name of a pymongo database
-            collection (str) name of a pymongo collection
+            host (str) hostname or IP address or Unix domain socket path
+            port (int) port number on which to connect
+            database (str) storage database
+            collection (str) storage collection
+            authSource (str|None) database to authenticate against
+                if None, defaults to the storage database
+            username (str|None) authentication username
+            password (str|None) authentication password
         '''
         Workspace.__init__(self)
-        self.path = path
+        self.host = host
+        self.port = port
         self.database = database
         self.collection = collection
+        self.authSource = authSource or database
+        self.username = username
+        self.password = password
 
     @remote_connection
     def to_storage(self, identifier, upsert=True):
@@ -83,14 +99,17 @@ class MongoFrame(Workspace):
                 if None then unique insertion is not enforced
             upsert (bool) insert missing documents in unique insertion mode
         '''
+
+        collection = self.connection[self.database][self.collection]
+
         if identifier:  # unique insertion mode
             for row in self.memory.to_dict(orient='records'):
-                self.connection.update_one(
+                collection.update_one(
                     filter={identifier: row[identifier]},
                     update={'$set': row},
                     upsert=upsert)
         else:  # documents are non-unique
-            self.connection.insert_many(
+            collection.insert_many(
                 self.memory.to_dict(orient='records'))
 
     @remote_connection
@@ -101,8 +120,11 @@ class MongoFrame(Workspace):
         args:
             **find (dict) optional arguments to pass to pymongo.find
         '''
+
+        collection = self.connection[self.database][self.collection]
+
         self.memory = DataFrame.from_records(
-            list(self.connection.find(**find)))
+            list(collection.find(**find)))
 
     @remote_connection
     def delete_storage(self, filter={}, clear_collection=False):
@@ -113,10 +135,13 @@ class MongoFrame(Workspace):
             filter (son) pymongo query operator passed to delete_many()
             clear_collection (bool) clear storage entirely
         '''
+
+        collection = self.connection[self.database][self.collection]
+
         if clear_collection:  # remove all documents
-            self.connection.delete_many({})
+            collection.delete_many({})
         elif filter:  # remove documents matching kwargs
-            self.connection.delete_many(filter)
+            collection.delete_many(filter)
         else:  # make sure collection purge is intended
             raise Exception('Do you mean to delete everything in {}.{}? If so,'
                             'then flag clear_collection as True.'.format(
